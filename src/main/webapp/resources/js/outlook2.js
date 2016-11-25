0ab7a00ca0ecb4c0635a7744271f5bd163dc4c70
YuHLin <326620938@qq.com>
2016-11-25 08:32:07 +0800

    添加easyui框架文件

---
+++ src/main/webapp/resources/js/outlook2.js
@@ -0,0 +1,268 @@
+﻿/**
+  START!
+**/
+var _menus = {"menus":[
+						{"menuid":"1","icon":"icon-role","menuname":"权限管理",
+							"menus":[
+									{"menuid":"12","menuname":"员工管理","icon":"icon-add","url":"http://blog.csdn.net/chinahuyong/article/details/11926719"},
+									{"menuid":"13","menuname":"用户管理","icon":"icon-users","url":"https://www.baidu.com/"},
+									{"menuid":"14","menuname":"角色管理","icon":"icon-role","url":"https://www.baidu.com/"},
+									{"menuid":"15","menuname":"权限管理","icon":"icon-set","url":"https://www.baidu.com/"},
+									{"menuid":"16","menuname":"公司管理","icon":"icon-log","url":"demo1.html"}
+								]
+						},{"menuid":"8","icon":"icon-users","menuname":"预约管理",
+							"menus":[{"menuid":"21","menuname":"预约数据","icon":"icon-nav","url":"demo.html"},
+									{"menuid":"22","menuname":"预约状态","icon":"icon-nav","url":"demo1.html"}
+								]
+						},{"menuid":"56","icon":"icon-magic","menuname":"访客管理",
+							"menus":[{"menuid":"31","menuname":"访客管理","icon":"icon-nav","url":"demo1.html"},
+									{"menuid":"32","menuname":"预约管理","icon":"icon-nav","url":"demo2.html"}
+								]
+						},{"menuid":"28","icon":"icon-nav","menuname":"黑白名单管理",
+							"menus":[{"menuid":"41","menuname":"黑名单","icon":"icon-nav","url":"demo.html"},
+									{"menuid":"42","menuname":"白名单","icon":"icon-nav","url":"demo1.html"},
+									{"menuid":"43","menuname":"SB名单","icon":"icon-nav","url":"demo2.html"}
+								]
+						}
+				]};
+
+				
+//设置登录窗口
+function openPwd() {
+    $('#w').window({
+        title: '修改密码',
+        width: 300,
+        modal: true,
+        shadow: true,
+        closed: true,
+        height: 160,
+        resizable:false
+         });
+}
+
+//关闭登录窗口
+function closePwd() {
+    $('#w').window('close');
+}
+
+        
+
+//修改密码
+function serverLogin() {
+    var $newpass = $('#txtNewPass');
+    var $rePass = $('#txtRePass');
+
+    if ($newpass.val() == '') {
+        msgShow('系统提示', '请输入密码！', 'warning');
+            return false;
+    }
+    if ($rePass.val() == '') {
+        msgShow('系统提示', '请在一次输入密码！', 'warning');
+        return false;
+    }
+
+    if ($newpass.val() != $rePass.val()) {
+        msgShow('系统提示', '两次密码不一至！请重新输入', 'warning');
+        return false;
+    }
+        msgShow('系统提示', '恭喜，密码修改成功！<br>您的新密码为：' + $newpass.val(), 'info');
+        closePwd();
+}
+	
+
+
+$(function(){
+	InitLeftMenu();
+	tabClose();
+	tabCloseEven();
+	openPwd();
+
+    $('#editpass').click(function() {
+        $('#w').window('open');
+    });
+
+    $('#btnEp').click(function() {
+        serverLogin();
+    })
+
+	$('#btnCancel').click(function(){closePwd();})
+
+    $('#loginOut').click(function() {
+        $.messager.confirm('系统提示', '您确定要退出本次登录吗?', function(r) {
+
+        if (r) {
+            ocation.href = '#';
+                }
+        });
+    })
+
+
+})
+
+//初始化左侧
+function InitLeftMenu() {
+	$("#nav").accordion({animate:false});
+
+    $.each(_menus.menus, function(i, n) {
+		var menulist ='';
+		menulist +='<ul>';
+        $.each(n.menus, function(j, o) {
+			menulist += '<li><div><a ref="'+o.menuid+'" href="#" rel="' + o.url + '" ><span class="icon '+o.icon+'" >&nbsp;</span><span class="nav">' + o.menuname + '</span></a></div></li> ';
+        })
+		menulist += '</ul>';
+
+		$('#nav').accordion('add', {
+            title: n.menuname,
+            content: menulist,
+            iconCls: 'icon ' + n.icon
+        });
+
+    });
+
+	$('.easyui-accordion li a').click(function(){
+		var tabTitle = $(this).children('.nav').text();
+
+		var url = $(this).attr("rel");
+		var menuid = $(this).attr("ref");
+		var icon = getIcon(menuid,icon);
+
+		addTab(tabTitle,url,icon);
+		$('.easyui-accordion li div').removeClass("selected");
+		$(this).parent().addClass("selected");
+	}).hover(function(){
+		$(this).parent().addClass("hover");
+	},function(){
+		$(this).parent().removeClass("hover");
+	});
+
+	//选中第一个
+	var panels = $('#nav').accordion('panels');
+	var t = panels[0].panel('options').title;
+    $('#nav').accordion('select', t);
+}
+//获取左侧导航的图标
+function getIcon(menuid){
+	var icon = 'icon ';
+	$.each(_menus.menus, function(i, n) {
+		 $.each(n.menus, function(j, o) {
+		 	if(o.menuid==menuid){
+				icon += o.icon;
+			}
+		 })
+	})
+
+	return icon;
+}
+
+function addTab(subtitle,url,icon){
+	if(!$('#tabs').tabs('exists',subtitle)){
+		$('#tabs').tabs('add',{
+			title:subtitle,
+			content:createFrame(url),
+			closable:true,
+			icon:icon
+		});
+	}else{
+		$('#tabs').tabs('select',subtitle);
+		$('#mm-tabupdate').click();
+	}
+	tabClose();
+}
+
+function createFrame(url)
+{
+	var s = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
+	return s;
+}
+
+function tabClose()
+{
+	/*双击关闭TAB选项卡*/
+	$(".tabs-inner").dblclick(function(){
+		var subtitle = $(this).children(".tabs-closable").text();
+		$('#tabs').tabs('close',subtitle);
+	})
+	/*为选项卡绑定右键*/
+	$(".tabs-inner").bind('contextmenu',function(e){
+		$('#mm').menu('show', {
+			left: e.pageX,
+			top: e.pageY
+		});
+
+		var subtitle =$(this).children(".tabs-closable").text();
+
+		$('#mm').data("currtab",subtitle);
+		$('#tabs').tabs('select',subtitle);
+		return false;
+	});
+}
+//绑定右键菜单事件
+function tabCloseEven()
+{
+	//刷新
+	$('#mm-tabupdate').click(function(){
+		var currTab = $('#tabs').tabs('getSelected');
+		var url = $(currTab.panel('options').content).attr('src');
+		$('#tabs').tabs('update',{
+			tab:currTab,
+			options:{
+				content:createFrame(url)
+			}
+		})
+	})
+	//关闭当前
+	$('#mm-tabclose').click(function(){
+		var currtab_title = $('#mm').data("currtab");
+		$('#tabs').tabs('close',currtab_title);
+	})
+	//全部关闭
+	$('#mm-tabcloseall').click(function(){
+		$('.tabs-inner span').each(function(i,n){
+			var t = $(n).text();
+			$('#tabs').tabs('close',t);
+		});
+	});
+	//关闭除当前之外的TAB
+	$('#mm-tabcloseother').click(function(){
+		$('#mm-tabcloseright').click();
+		$('#mm-tabcloseleft').click();
+	});
+	//关闭当前右侧的TAB
+	$('#mm-tabcloseright').click(function(){
+		var nextall = $('.tabs-selected').nextAll();
+		if(nextall.length==0){
+			msgShow('系统提示','后边没有啦~~','error');
+			//alert('后边没有啦~~');
+			return false;
+		}
+		nextall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+			$('#tabs').tabs('close',t);
+		});
+		return false;
+	});
+	//关闭当前左侧的TAB
+	$('#mm-tabcloseleft').click(function(){
+		var prevall = $('.tabs-selected').prevAll();
+		if(prevall.length==0){
+			msgShow('系统提示','前边没有啦~~','error');
+			//alert('到头了，前边没有啦~~');
+			return false;
+		}
+		prevall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+			$('#tabs').tabs('close',t);
+		});
+		return false;
+	});
+
+	//退出
+	$("#mm-exit").click(function(){
+		$('#mm').menu('hide');
+	})
+}
+
+//弹出信息窗口 title:标题 msgString:提示信息 msgType:信息类型 [error,info,question,warning]
+function msgShow(title, msgString, msgType) {
+	$.messager.alert(title, msgString, msgType);
+}
