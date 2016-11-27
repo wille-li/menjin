25081b8a3ca1caef622d5596e7deff46e9d6fa69
Wille <IT.wille@gmail.com>
2016-11-27 11:46:11 +0800

    上传图片Model Mapper

---
+++ src/main/java/com/menjin/photo/mapper/PhotoInfoMapper.java
@@ -0,0 +1,8 @@
+package com.menjin.photo.mapper;
+
+import com.base.mapper.BaseCrudMapper;
+import com.menjin.photo.model.PhotoInfo;
+
+public interface PhotoInfoMapper extends BaseCrudMapper<PhotoInfo>{
+
+}
