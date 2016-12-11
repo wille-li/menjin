cf19bf1b7b33985a882e7bd98c08918f2357b12f
jack-huang2016 <2456120975@qq.com>
2016-12-11 10:10:58 +0800

    解决Resource冲突

--- src/main/java/com/menjin/user/controller/ResourceControler.java
+++ src/main/java/com/menjin/user/controller/ResourceControler.java
@@ -1,34 +1,24 @@
 package com.menjin.user.controller;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.base.annotation.log.SystemControllerLog;
 import com.base.entity.SimplePage;
 import com.menjin.user.model.Resource;
-import com.menjin.user.model.Role;
-import com.menjin.user.model.User;
 import com.menjin.user.service.If.ResourceServiceIf;
-import com.menjin.user.service.If.RoleServiceIf;
-import com.menjin.user.service.If.UserServiceIf;
 
 @Controller
 public class ResourceControler {
@@ -64,10 +54,104 @@ public class ResourceControler {
 		return maps;
 	}
 	
+	@RequestMapping(value="/resource/deleteResource.do")
+	@SystemControllerLog
+	@ResponseBody
+	public Integer deleteResource(@ModelAttribute Resource resource){
+		logger.info("Start to delete resource,resourceUrl="+resource.getResourceUrl());
+		int returnCode = resourceService.deleteById(resource);
+		logger.info("End to delete resource,returnCode:"+returnCode);
+		return returnCode;
+	}
+	
+	@RequestMapping(value="/resource/addResource.do")
+	@SystemControllerLog
+	@ResponseBody
+	public Integer addResource(@ModelAttribute Resource resource){
+		logger.info("Start to add new resource,resourceUrl="+resource.getResourceUrl());
+		int returnCode = resourceService.add(resource);
+		logger.info("End to add resource,ReturnCode="+returnCode);
+		return returnCode;
+	}
+	
+	@RequestMapping(value="/resource/updateResource.do")
+	@SystemControllerLog
+	@ResponseBody
+	public Integer updateResource(@ModelAttribute Resource resource){
+		logger.info("Start to update resource!");
+		int returnCode = resourceService.modifyById(resource);
+		logger.info("End to update resource,ReturnCode:"+returnCode);
+		return returnCode;
+	}
+	
+	@RequestMapping(value="/resource/getParentResources.do")
+	@SystemControllerLog
+	@ResponseBody
+	public String getParentResources(){
+		logger.info("Start to getParentResources");
+		List<Resource> list = resourceService.findParent();
+		String str = "";
+		if(list!=null&&list.size()>0){
+			str = JSON.toJSONString(list);
+			logger.info("json tranfer="+str);
+		}
+		logger.info("End getParentResources");
+		return str;
+	}
+	
+	/**
+	   * 将权限转换成tree格式、
+	   * @param privileges
+	   * @return
+	   */
+	  private Map<String, List<Map<String, String>>> trimPrivileges(List<Resource> privileges){
+	    	Map<String, List<Map<String, String>>> result = new HashMap<String, List<Map<String, String>>>();
+	    	for (Resource privilege : privileges) {
+				 String catalog = String.valueOf(privilege.getParentId());
+				 Map<String, String> node = new HashMap<String, String>();
+				 node.put("id",privilege.getId().toString());
+				 node.put("text", privilege.getName());
+				 if(result.get(catalog)!=null){
+					 List<Map<String, String>> childrens = result.get(catalog);
+					 childrens.add(node);
+					 result.put(catalog, childrens);
+				 }else{
+					 List<Map<String, String>> childrens = new ArrayList<Map<String, String>>();
+					 childrens.add(node);
+					 result.put(catalog, childrens);
+					 
+				 }
+			}
+	    	return result;
+	    }
+	  
+	  @ResponseBody
+	    @RequestMapping(value="/resource/getResourceNodes.do")
+		/**
+		 * 获取权限列表
+		 * @return
+		 */
+	    public List<Map<String, Object>> getPrivilegeNodes(){
+	        List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
+	        int count = resourceService.findCount(null, null);
+			logger.info("Resources Count:"+count);
+			SimplePage simplepage = new SimplePage(1, count, count);
+			Map<String, Object> params = null;
+			String orderBy = "parentId";
+			List<Resource> resources = resourceService.findByPage(simplepage, params, orderBy);
+	        Map<String, List<Map<String, String>>> privileges = trimPrivileges(resources);
+	        
+	        for (Map.Entry<String, List<Map<String, String>>> privilege : privileges.entrySet()) {
+	            Map<String, Object> node = new HashMap<String, Object>();
+	            node.put("id", "");
+	            node.put("text", privilege.getKey());
+	            node.put("children", privilege.getValue());
+	            results.add(node);
+	        }
+	        return results;
+	    }
+	
 	/*
-	
-
-	
 	@RequestMapping(value="/user/getUserByUsername.do")
 	@SystemControllerLog
 	@ResponseBody
@@ -115,46 +199,5 @@ public class ResourceControler {
 		logger.info("End getUserByRolename");
 		return maps;
 	}
-	
-	@RequestMapping(value="/user/addUser.do")
-	@SystemControllerLog
-	@ResponseBody
-	public Integer addUser(@ModelAttribute User user,
-			HttpServletRequest request){
-		logger.info("Start to add new user,username="+user.getUsername());
-		String currentLoginUser = getCurrentUsername(request);
-		logger.info("当前登录用户为："+currentLoginUser+", 操作：新增用户");
-		Date date = Calendar.getInstance().getTime();
-		user.setCreatedBy(currentLoginUser);
-		user.setModifiedBy(currentLoginUser);
-		user.setModifiedDate(date);
-		user.setCreatedDate(date);
-		int returnCode = userService.add(user);
-		logger.info("End to add user,ReturnCode="+returnCode);
-		return returnCode;
-	}
-	
-	@RequestMapping(value="/user/updateUser.do")
-	@SystemControllerLog
-	@ResponseBody
-	public Integer updateUser(@ModelAttribute User user,HttpServletRequest request){
-		logger.info("Start to update user!");
-		String currentLoginUser = getCurrentUsername(request);
-		logger.info("当前登录用户为："+currentLoginUser+", 操作：修改用户");
-		user.setModifiedDate(Calendar.getInstance().getTime());
-		user.setModifiedBy(currentLoginUser);
-		int returnCode = userService.modifyById(user);
-		logger.info("End to update user!ReturnCode:"+returnCode);
-		return returnCode;
-	}
-	
-	@RequestMapping(value="/user/deleteUser.do")
-	@SystemControllerLog
-	@ResponseBody
-	public Integer deleteUser(@ModelAttribute User user,HttpServletRequest request){
-		logger.info("Start to delete user,username="+user.getUsername());
-		int returnCode = userService.deleteById(user);
-		logger.info("End to delete user,retrunCode:"+returnCode);
-		return returnCode;
-	}*/
+	*/
 }
